0cff6c5b990eb96f95ad7e4819184edc54597abf
Eliane Henriqueta <elianehenriqueta@gmail.com>
2023-08-29 13:32:26 -0300

    feat: Revisao do projeto

--- HETech.API/Config/ServiceIoc.cs
+++ HETech.API/Config/ServiceIoc.cs
@@ -1,6 +1,17 @@
-﻿namespace HETech.API.Config
+﻿using HETech.Domain.Interfaces.Services;
+using HETech.Domain.Services;
+
+namespace HETech.API.Config
 {
     public class ServiceIoc
     {
+        public static void RegisterServices(IServiceCollection builder)
+        {
+            builder.AddScoped<IUsuarioService, UsuarioService>();
+            builder.AddScoped<ICategoriaService, CategoriaService>();
+            builder.AddScoped<IProdutoService, ProdutoService>();
+            builder.AddScoped<IProdutoVendaService, ProdutoVendaService>();
+            builder.AddScoped<IVendaService, VendaService>();
+        }
     }
 }
